68a4dead1eb47d228fad32e04fe758584c647fb1
taxdevelopment <music.taxtm@gmail.com>
2025-09-05 00:51:04 +0200

    Update animated-header.tsx

--- src/components/navigation/animated-header.tsx
+++ src/components/navigation/animated-header.tsx
@@ -13,9 +13,9 @@ import HeaderLoginButton from "./HeaderLoginButton";
 export const defaultNavigation = {
   items: config.navigation?.items ?? [
     { label: "About", href: "#about", enabled: true },
-    { label: "Servers", href: "#servers", enabled: true },
-    { label: "Leaderboard", href: "#leaderboard", enabled: config.cftools.leaderboard.enabled },
-    { label: "Contact", href: "#contact", enabled: true },
+    // { label: "Servers", href: "#servers", enabled: true },
+    // { label: "Leaderboard", href: "#leaderboard", enabled: config.cftools.leaderboard.enabled },
+    // { label: "Contact", href: "#contact", enabled: true },
   ],
 } satisfies { items: NavItem[] };
 
